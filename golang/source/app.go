55a644ed627d56c61ab3be7039684de028d82631
nantawat chairat <nantawat@opsta.co.th>
2022-05-19 10:39:54 +0700

    update model

--- golang/source/app.go
+++ golang/source/app.go
@@ -4,13 +4,24 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+type Book struct {
+	Title      string    `bson:"title,omitempty"`
+	Created_at time.Time `bson:"created_at"`
+}
+
 func main() {
+	fmt.Println("run main function")
+	bson_collection()
+}
+
+func bson_collection() {
 	var ctx = context.TODO()
 
 	clientOptions := options.Client().ApplyURI("mongodb://root:root@mongodb:27017/")
@@ -22,22 +33,25 @@ func main() {
 		fmt.Println("Connected to MongoDB")
 	}
 
-	// insert database and collection with date time
-	collection := client.Database("golang").Collection("golang")
+	collection := client.Database("golang").Collection("bson_db")
 
-	// insert data title, content, created_at
-	user := []interface{}{
-		bson.D{{"type", "English Breakfast"}, {"rating", 6}},
-		bson.D{{"type", "Oolong"}, {"rating", 7}, {"vendor", bson.A{"C"}}},
-		bson.D{{"type", "Assam"}, {"rating", 5}},
-		bson.D{{"type", "Earl Grey"}, {"rating", 8}, {"vendor", bson.A{"A", "B"}}},
-	}
+	user := bson.D{{"title", "The Room"}, {"type", "English Breakfast"}, {"rating", 6}, {"created_at", time.Now()}}
 
-	result, err := collection.InsertMany(context.TODO(), user)
+	result, err := collection.InsertOne(context.TODO(), user)
 
 	if err != nil {
 		panic(err)
 	}
-	// display the id of the newly inserted object
 	fmt.Println(result)
+	fmt.Println("completed insert bson record")
+
+	var books []Book
+	cursor, err := collection.Find(context.TODO(), bson.D{{"title", "The Room"}})
+	cursor.All(ctx, &books)
+
+	for i := range books {
+		fmt.Println(books[i].Title)
+		fmt.Println(books[i].Created_at.Format("2006-02-01"))
+	}
+
 }
